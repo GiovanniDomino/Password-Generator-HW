240a8f3e86119c6c632e58302f116a49c4cd0a68
Giovanni <gdomino01@gmail.com>
2021-11-06 14:12:42 -0600

    started the randomizer for the password

--- Develop/script.js
+++ Develop/script.js
@@ -10,4 +10,15 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
+function generatePassword() {
+  var length = Number(prompt("How long would you like your password be? Enter a number between 8 and 128"));
+  
+  var length = 128,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
